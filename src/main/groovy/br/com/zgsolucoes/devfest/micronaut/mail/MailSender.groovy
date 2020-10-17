b0ecc662c7cfebddacf3bf95523ad7186ff0925d
João Paulo Ozório da Silva <joaopaulo@zgsolucoes.com.br>
2020-10-17 11:18:00 -0300

    melhora email

--- src/main/groovy/br/com/zgsolucoes/devfest/micronaut/mail/MailSender.groovy
+++ src/main/groovy/br/com/zgsolucoes/devfest/micronaut/mail/MailSender.groovy
@@ -5,73 +5,83 @@ import com.sendgrid.*
 
 class MailSender {
 
-	ResultadoSorteio resultadoSorteio
+    ResultadoSorteio resultadoSorteio
 
-	final String conteudo = """
-Confira nossas vagas:    https://materiais.zgsolucoes.com.br/zgteam
+    final static String conteudo = """
+<div>
+    <div>
+        <a href="https://materiais.zgsolucoes.com.br/zgteam">Confira nossas vagas</a>
+    </div>
 
-ZG Cast: https://anchor.fm/zgsolucoes
-https://www.deezer.com/us/show/1206822
+    <div>
+        <a href="https://anchor.fm/zgsolucoes">ZG Cast</a>
+        <a href="https://www.deezer.com/us/show/1206822">ZG Cast - Deezer</a>
+    </div>
 
-ZG Soluções: https://zgsolucoes.com.br/
+    <div>
+        <a href="https://zgsolucoes.com.br/">Conheça nosso site</a>
+    </div>
 
-Linktree: https://linktr.ee/zgsolucoes
+    <div>
+        <a href="https://linktr.ee/zgsolucoes">Linktree</a>
+    </div>
+</div>
 """
 
-	MailSender(ResultadoSorteio resultadoSorteio) {
-		this.resultadoSorteio = resultadoSorteio
-	}
+    MailSender(ResultadoSorteio resultadoSorteio) {
+        this.resultadoSorteio = resultadoSorteio
+    }
 
-	void avisaTodos() {
-		avisaVencedor()
-		avisaNaoVencedores()
-	}
+    void avisaTodos() {
+        avisaVencedor()
+        avisaNaoVencedores()
+    }
 
-	private void avisaVencedor() {
-		Email from = new Email("joaopaulo@zgsolucoes.com.br", 'João Paulo Ozório')
+    private void avisaVencedor() {
+        Email from = new Email("joaopaulo@zgsolucoes.com.br", 'João Paulo Ozório')
 
-		String subject = "Parabéns! Você foi o vencedor!"
-		Email to = new Email(resultadoSorteio.vencedor)
-		Content content = new Content("text/plain", "Parabéns! Você foi o vencedor!".concat(conteudo))
+        String subject = "Parabéns! Você foi o vencedor!"
+        Email to = new Email(resultadoSorteio.vencedor)
+        Content content = new Content("text/html", "<h2>Parabéns! Você foi o vencedor!</h2>".concat(conteudo))
 
-		Mail mail = new Mail(from, subject, to, content)
+        Mail mail = new Mail(from, subject, to, content)
 
-		Personalization personalization = mail.personalization.get(0)
-		personalization.addCc(new Email('joaopaulo@zgsolucoes.com.br'))
+        Personalization personalization = mail.personalization.get(0)
+        personalization.addCc(new Email('joaopaulo@zgsolucoes.com.br'))
 
-		SendGrid sg = new SendGrid(System.getenv('SENDGRID_ENV'))
-		Request request = new Request()
-		try {
-			request.setMethod(Method.POST)
-			request.setEndpoint("mail/send")
-			request.setBody(mail.build())
-			sg.api(request)
-		} catch (IOException ex) {
-			throw ex
-		}
-	}
+        SendGrid sg = new SendGrid(System.getenv('SENDGRID_ENV'))
+        Request request = new Request()
+        try {
+            request.setMethod(Method.POST)
+            request.setEndpoint("mail/send")
+            request.setBody(mail.build())
+            sg.api(request)
+        } catch (IOException ex) {
+            throw ex
+        }
+    }
 
-	private void avisaNaoVencedores() {
-		Email from = new Email("joaopaulo@zgsolucoes.com.br", 'João Paulo Ozório')
-		Content content = new Content("text/plain", "Você não foi sorteado.".concat(conteudo))
-		String subject = "Você não foi sorteado"
+    private void avisaNaoVencedores() {
+        Email from = new Email("joaopaulo@zgsolucoes.com.br", 'João Paulo Ozório')
+        Content content = new Content("text/html", "<h2>Não foi dessa vez</h2>".concat(conteudo))
+        String subject = "Que pena, não foi dessa vez."
 
-		resultadoSorteio.naoVencedores.each { String email ->
-			Email to = new Email(email)
+        resultadoSorteio.naoVencedores.each { String email ->
+            Email to = new Email(email)
 
-			Mail mail = new Mail(from, subject, to, content)
+            Mail mail = new Mail(from, subject, to, content)
 
-			SendGrid sg = new SendGrid(System.getenv('SENDGRID_ENV'))
-			Request request = new Request()
-			try {
-				request.setMethod(Method.POST)
-				request.setEndpoint("mail/send")
-				request.setBody(mail.build())
-				sg.api(request)
-			} catch (IOException ex) {
-				ex.printStackTrace()
-			}
-		}
-	}
+            SendGrid sg = new SendGrid(System.getenv('SENDGRID_ENV'))
+            Request request = new Request()
+            try {
+                request.setMethod(Method.POST)
+                request.setEndpoint("mail/send")
+                request.setBody(mail.build())
+                sg.api(request)
+            } catch (IOException ex) {
+                ex.printStackTrace()
+            }
+        }
+    }
 
 }
